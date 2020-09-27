fcce59b97e5c0f8de1ed0b355cff7bbcfc8070c4
HexadecimalHUN <sajtos0007@gmail.com>
2020-09-27 09:41:00 -0700

    Add files via upload

---
+++ FilmClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data.SqlTypes;
+using System.Data;
+
+
+namespace LogicTier
+{
+    public class FilmData
+    {
+        public DataSet GetFilmDB(string connectionString)
+        {
+            string selectSQL = "";
+
+            SqlConnection con = new SqlConnection(connectionString);
+            SqlDataAdapter da = new SqlDataAdapter(selectSQL, con);
+
+            DataSet ds = new DataSet();
+
+            return ds;
+
+            
+        }
+    }
+}
